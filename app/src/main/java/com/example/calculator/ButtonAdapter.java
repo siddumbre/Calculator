e2fe8160029251e2d6d15cc9d555ddfc87cc9b1d
siddharth.dumbre <siddharth.dumbre@ril.com>
2022-11-25 18:45:06 +0530

    Replaced Button with Textview

--- app/src/main/java/com/example/calculator/ButtonAdapter.java
+++ app/src/main/java/com/example/calculator/ButtonAdapter.java
@@ -36,18 +36,18 @@ public class ButtonAdapter extends BaseAdapter {
         ViewHolder viewHolder = new ViewHolder();
         view=View.inflate(activity,R.layout.btn_layout,null);
 //        Button title = view.findViewById(R.id.title);
-        viewHolder.title=(Button) view.findViewById(R.id.title);
+        viewHolder.title=(TextView) view.findViewById(R.id.title);
         view.setTag(viewHolder);
         viewHolder=(ViewHolder)view.getTag();
         viewHolder.title.setText(ButtonText.get(i));
         if(ButtonText.get(i).contains("C")){
-            viewHolder.title.setBackgroundColor(view.getResources().getColor(R.color.grey));
+            viewHolder.title.setTextColor(view.getResources().getColor(R.color.orange));
         }
 //        title.setText(ButtonText.get(i));
         return view;
     }
 
     private static class ViewHolder{
-        public Button title;
+        public TextView title;
     }
 }
